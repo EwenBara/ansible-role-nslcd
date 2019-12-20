0ada0642e0ada6f115fe5a1590b97a25414ee774
Ewen BARA <ewen.bara@gmail.com>
2019-12-20 11:53:06 +0100

    Add test to check the role is applied correctly

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -8,8 +8,14 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 
 def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+    assert host.package("nss-pam-ldapd").is_installed
 
+    f = host.file('/etc/nslcd.conf')
     assert f.exists
     assert f.user == 'root'
     assert f.group == 'root'
+    assert f.mode == 0o600
+
+    s = host.service("nslcd")
+    assert s.is_running
+    assert s.is_enabled
